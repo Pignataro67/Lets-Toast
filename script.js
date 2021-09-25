4bdf2bd77ec6af8510b5d5489ee6089452d3bc7a
Pignataro67 <Pignataro67@gmail.com>
2021-09-25 04:53:38 -0700

    Adding JS

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ const messages = [
   'Message Three',
   'Message Four',
 ]
+
+const types = ['info', 'success', 'error']
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification(message = null, type = null) {
+  const notif = document.createElement('div')
+  notif.classList.add('toast')
+  notif.classList.add(type ? type : getRandomType())
+
+  notif.innerText = message ? message : getRandomMessage()
+
+  toasts.appendChild(notif)
+
+  setTimeout(() => {
+      notif.remove()
+  }, 3000)
+}
